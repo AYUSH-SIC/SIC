6cfcf681fe26544664afc460faf063edf9ad301f
AYUSH-SIC <ayushisernsit@gmail.com>
2025-02-18 12:46:27 +0530

    ADDED PATTERN QUESTION IN RANGE.PY

---
+++ PYTHON/DAY2/range.py
@@ -0,0 +1,10 @@
+input_number = int(input('Enter a small number of your choice : '))
+for i in range(input_number):
+    i += 2
+    print(i)
+
+''' Find the nth term of the series : 1 2 2 3 3 5 5 7 8 11 13 13 
+     answer :- 
+        odd terms : 1 2 3 5 8 13 21 (fibbonacci series [starting from 1])
+        even term : 2 3 5 7 11 13 (prime numbers )
+'''
