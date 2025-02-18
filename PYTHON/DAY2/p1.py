9f513fccb9c3bde7e3e64f630fe99cff0c8f9f94
AYUSH-SIC <ayushisernsit@gmail.com>
2025-02-18 14:33:02 +0530

    18-2

--- PYTHON/DAY2/p1.py
+++ PYTHON/DAY2/p1.py
@@ -4,8 +4,12 @@
 input_number = input("ENTER THE NUMBER : ")
 input_list = list(input_number)
 input_rev = input_list[::-1]
+index = 0
 for i in range(len(input_list) - 1):
     if (input_rev[i] > input_rev[i+1]):
         input_rev[i],input_rev[i+1] = input_rev[i+1],input_rev[i]
+        index = i
         break
-print(''.join(input_rev[::-1]))
+index = len(input_list) - 2
+input_rev = input_rev[::-1]
+print(''.join(input_rev[0:index] + sorted(input_rev[index:])))
